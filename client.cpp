5c69101301327e753da9b2733debd000fb6a7353
diwas <lunaticoda123@gmail.com>
2024-02-23 16:48:21 +0545

    Built a working client and server class

--- client.cpp
+++ client.cpp
@@ -23,27 +23,44 @@ class Client{
         struct sockaddr_in server_address ;
         struct hostent * server ;
         char buffer[MAX] ;
+        int sockfd ;
+        int status ;
         int port ;
 
     public:
         Client(int port_num){
-
+            std::memset((char*) &server_address, 0, sizeof(server_address)) ;
+            sockfd = socket(AF_INET, SOCK_STREAM, 0);
+            if (sockfd < 0) // sockfd == -1
+                error("Can't create socket for connection") ;
+            server_address.sin_family = AF_INET ; // Internet Domain Network
+            server_address.sin_port = htons(port_num); // Convert to network order bytes
+            this->port = port_num ;
         }
 
         void connectWithServer(const char* host_name){
-
+            server = gethostbyname(host_name) ;
+            if(server == NULL)
+                error("Can't find the host - Host doesn't exist") ;
+            std::memcpy((struct sockaddr_in*) &server_address.sin_addr.s_addr, server->h_addr, server->h_length) ;
         }
 
         void sendRequest(std::string request){
-
+            status = write(sockfd, request.c_str(), request.length());
+            if(status < 0)
+                error("Can't send request to the server");
         }
 
         void getResponse(){
-
+            std::memset(buffer, 0, sizeof(buffer)) ;
+            status = read(sockfd, buffer, sizeof(buffer)-1);
+            if (status < 0)
+                error("Can't fetch response from the server");
+            std::cout << "Server: " << buffer << std::endl ;
         }
 
         void terminateClient(){
-
+            close(sockfd) ;
         }
 } ;
 
--- server.cpp
+++ server.cpp
@@ -20,37 +20,58 @@ void error(std::string message){
 class Server{
     private:
         struct sockaddr_in server_address, client_address ;
+        int sockfd, cli_sockfd ;
         char buffer[MAX] ;
         int status ;
         int port ;
 
     public:
         Server(int port_num){
-
+            std::memset((char*) &server_address, 0, sizeof(server_address)) ;
+            sockfd = socket(AF_INET, SOCK_STREAM, 0);
+            if (sockfd < 0) // sockfd == -1
+                error("Can't create socket for connection") ;
+            server_address.sin_addr.s_addr = INADDR_ANY ; // Server address of machine 
+            server_address.sin_family = AF_INET ; // Internet Domain Network
+            server_address.sin_port = htons(port_num); // Convert to network order bytes
+            this->port = port_num ;
         }
 
         void initialize(){
-
+            if(bind(sockfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) 
+                error("Can't bind socket with socket address");
         }
 
         void getServerInfo(){
-
+            std::cout << "Server Address = " << server_address.sin_addr.s_addr << ":" << this->port << std::endl ;
         }
 
         void connectWithClient(int maxQueue = 5){
-
+            if(listen(sockfd, maxQueue) < 0)
+                error("Can't find any client requests") ;
+            socklen_t length = sizeof(client_address) ;
+            cli_sockfd = accept(sockfd, (struct sockaddr *) &client_address, &length) ;
+            if (cli_sockfd < 0) 
+                error("Can't accept connection from client at " + client_address.sin_addr.s_addr);
         }
 
         void getRequest(){
-
+            std::memset(buffer, 0, sizeof(buffer)) ;
+            status = read(cli_sockfd, buffer, sizeof(buffer)-1);
+            if (status < 0)
+                error("Can't fetch request from client");
+            std::cout << "Client: " << buffer << std::endl ;
         }
 
         void sendResponse(std::string response){
-
+            status = write(cli_sockfd, response.c_str(), response.length());
+            if(status < 0)
+                error("Can't send response to client");
         }
 
         void terminateServer(){
-
+            close(cli_sockfd) ;
+            close(sockfd) ;
         }
 } ;
 
